ae6afb075981fccb33c55875ce3a10ef28b42965
Guilherme-C808 <gui.oliv.cardoso@gmail.com>
2025-11-28 11:24:44 +0000

    Update analysis_general.py

--- src/analysis_general.py
+++ src/analysis_general.py
@@ -9,8 +9,13 @@ def reviews_by_year(data):
         
         if data_legivel:
             # TAREFA: Extrair apenas o ano (os primeiros 4 caracteres)
+            ano = data_legivel[:4]
             # TAREFA: Contar quantas reviews há nesse ano
-            pass
+            ano = data_legivel[:4]
+            if ano not in reviews_por_ano:
+                reviews_por_ano[ano] = 0
+            reviews_por_ano[ano] += 1
+
     return reviews_por_ano
 
 def analyze_word_frequency(data):
@@ -18,8 +23,8 @@ def analyze_word_frequency(data):
     words_5_star = {}
     words_1_star = {}
     
-    # Palabras para ignorar (podes adicionar mais)
-    stop_words = ['the', 'and', 'a', 'to', 'of', 'is', 'it', 'in', 'i', 'this']
+    # Palavras para ignorar (podes adicionar mais)
+    stop_words = ['the', 'and', 'a', 'to', 'of', 'is', 'it', 'in', 'i', 'this', 'that', 'was', 'for', 'on', 'with', 'as', 'but', 'are', 'they', 'be', 'at', 'or']
     
     for row in data:
         score = row['Score']
